e3b7b0ffbf72c0be076ef909b2072ecb5acc2962
Yaqoob M <yaqoobmmaniar@gmail.com>
2024-01-16 23:01:36 +0000

    updated url for rest auth

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 urlpatterns = [
     path("", include("frontend.urls")),
     path("api/", include("api.urls")),  # api end points
+    path("api/auth/", include("rest_framework.urls")),  # for rest browsable api
     path("admin/", admin.site.urls),  # admin panel
     path("__debug__/", include("debug_toolbar.urls")),  # browser debug toolbar
-    path("api-auth/", include("rest_framework.urls")),  # for rest browsable api
 ]
