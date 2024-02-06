e4bfc0920ba5dc0c1819a9a67bc49b43421fdd71
Yaqoob M <yaqoobmmaniar@gmail.com>
2024-02-06 23:10:20 +0000

    navbar

--- react/src/components/page/navigation-bar/NavigationBar.tsx
+++ react/src/components/page/navigation-bar/NavigationBar.tsx
@@ -26,8 +26,6 @@ const NavigationBar: FC = () => {
   let backgroundColor: string =
     "bg-gray-800 bg-gradient-to-r from-gray-100 to-gray-200/95 dark:from-gray-800 dark:bg-gray-500 dark:to-gray-900";
 
-  let linkColor: string = "hover:text-amber-500 hover:dark:text-amber-300";
-
   return (
     <nav
       className={`relative z-20 flex flex-row items-center justify-between px-8 py-3 text-gray-950 shadow dark:text-gray-200 dark:shadow-lg ${backgroundColor}`}
@@ -35,38 +33,41 @@ const NavigationBar: FC = () => {
       <Link to="/home" className="text-3xl font-medium">
         Dictate
       </Link>
-      <span className="flex flex-row items-center justify-between">
+      <span className="flex flex-row items-center justify-between gap-x-4">
         <MenuButton
           width="24"
           className={`peer stroke-gray-950 hover:cursor-pointer dark:stroke-gray-200 lg:hidden ${toggleMenu ? "clicked" : ""}`}
           onClick={() => setToggleMenu((prev) => !prev)}
         />
         <span
-          className={`group hidden items-center gap-x-8 gap-y-2 peer-[.clicked]:absolute peer-[.clicked]:left-0 peer-[.clicked]:top-full peer-[.clicked]:z-10 peer-[.clicked]:flex peer-[.clicked]:w-screen peer-[.clicked]:flex-col peer-[.clicked]:pb-4 peer-[.clicked]:shadow-xl lg:!static lg:flex lg:!w-fit lg:!flex-row lg:!bg-transparent lg:!bg-none lg:!pb-0 lg:!shadow-none ${toggleMenu ? backgroundColor + " clicked" : ""}`}
+          className={`hidden items-center gap-x-8 gap-y-2 peer-[.clicked]:absolute peer-[.clicked]:left-0 peer-[.clicked]:top-full peer-[.clicked]:z-10 peer-[.clicked]:flex peer-[.clicked]:w-screen peer-[.clicked]:flex-col peer-[.clicked]:pb-4 peer-[.clicked]:shadow-xl lg:!static lg:flex lg:!w-fit lg:!flex-row lg:!bg-transparent lg:!bg-none lg:!pb-0 lg:!shadow-none ${toggleMenu ? backgroundColor : ""}`}
         >
-          <span className="flex items-center gap-x-10 gap-y-2 border-current group-[.clicked]:flex-col group-[.clicked]:border-r-0 group-[.clicked]:pr-0 lg:!flex-row lg:!border-r lg:!pr-8">
-            <Link className={`font-medium ${linkColor}`} to="/login">
-              Login
-            </Link>
-            <Link className={`font-medium ${linkColor}`} to="/signup">
-              Sign Up
-            </Link>
-          </span>
-          <LightModeIcon
-            width="30"
-            className="stroke-amber-400 dark:stroke-amber-300"
-          />
+          <Link
+            className="font-medium hover:text-amber-500 hover:dark:text-amber-300"
+            to="/login"
+          >
+            Login
+          </Link>
+          <Link
+            className="font-medium hover:text-amber-500 hover:dark:text-amber-300"
+            to="/signup"
+          >
+            Sign Up
+          </Link>
+        </span>
+        {currentTheme === "dark" ? (
           <DarkModeIcon
             height="23"
-            className="stroke-amber-400 dark:stroke-amber-300"
-          />
-          <button
+            className="cursor-pointer stroke-amber-400 dark:stroke-amber-300"
             onClick={toggleTheme}
-            className={`border-b border-current ${linkColor}`}
-          >
-            {currentTheme === "dark" ? "Light Mode" : "Dark Mode"}
-          </button>
-        </span>
+          />
+        ) : (
+          <LightModeIcon
+            width="30"
+            className="cursor-pointer stroke-amber-400 dark:stroke-amber-300"
+            onClick={toggleTheme}
+          />
+        )}
       </span>
     </nav>
   );
