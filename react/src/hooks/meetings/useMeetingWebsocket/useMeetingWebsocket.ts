694a0ce7435fa407bc1a767170b50905c81d4e3c
Yaqoob M <yaqoobmmaniar@gmail.com>
2024-02-20 19:32:00 +0000

    fixed unknown stun url bug

--- react/src/hooks/meetings/useMeetingWebsocket/useMeetingWebsocket.ts
+++ react/src/hooks/meetings/useMeetingWebsocket/useMeetingWebsocket.ts
@@ -56,10 +56,7 @@ const getRelayServers = () => {
     // todo: add public domain name
     iceServers.push({ urls: "stun:stun.localhost:3478" });
   }
-  iceServers.push(
-    { urls: "stun:stun.l.google.com:19302" },
-    { urls: "stun:global.stun.twilio.com:3478?transport=udp" },
-  );
+  iceServers.push({ urls: "stun:stun.l.google.com:19302" });
   return iceServers;
 };
 
